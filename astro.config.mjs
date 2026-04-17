2327600359ae2d08e302d209f82a150e7f5c2dc8
Ed Diggs <diggs.it.accnt@gmail.com>
2026-04-17 21:50:28 +0200

    asdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-   outDir: './docs',
+  site: 'https://adiggstion.github.io',
+  base: '/TheSchneiderHair/',
+  outDir: './docs',
   vite: {
     plugins: [tailwindcss()]
   }
